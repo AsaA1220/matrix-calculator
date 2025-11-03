a30f4649c4e6ac51b8a4b244e07805ce29e912f9
AsaA1220 <tatisha302@gmail.com>
2025-11-03 16:59:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function createMatrix() {
-  
+  const rows = parseint(document.getElementById('rows').value);
+  const cols = parseint(document.getElementById('cols').value);
+
+  if (rows < 1 || rows > 10 || cols < 1 || cols > 10) {
+    alert('Размер матрицы должен быть оь 1 до 10');
+    return;
+  }
+  const container
 }
 
