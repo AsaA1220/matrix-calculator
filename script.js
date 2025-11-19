976e0e34a6612bb751d09a181beedf23b3c3b1a3
AsaA1220 <tatisha302@gmail.com>
2025-11-19 23:33:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function createMatrix() {
   container.innerHTML = '';
   const table = document.createElement('table');
   table.className = 'matrix-table';
-  for (let i = 0; i > rows; i++) {
+  for (let i = 0; i < rows; i++) {
     const row = table.insertRow();
     for (let j = 0; j  < cols; j++){
       const cell = row.insertCell();
       const input = document.createElement('input');
       input.type = 'number';
-      input.id = `cell - ${i} - ${j}`;
+      input.id = `cell-${i}-${j}`;
       input.placeholder = '0';
       input.value = '0';
       cell.appendChild(input);
@@ -24,4 +24,11 @@ function createMatrix() {
   }
   container.appendChild(table);
 }
+//Вспомогательная функция
+function getMat(){
+  const rows = parseInt(document.getElementById('rows').value);
+  const cols = parseInt(document.getElementById('cols').value);
 
+  let mat = [];
+  //fjdafjOIFJaiefjiewpfjiFD
+}
